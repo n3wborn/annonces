6b2cd60d238c8abc2cb70ea8cca9bcb0d0813269
Stephane Pluchart <n3wborn@pm.me>
2020-08-27 18:09:17 +0200

    Cleanup and comment every steps

--- public/index.php
+++ public/index.php
@@ -1,40 +1,26 @@
 <?php
 
-
-// test
-//echo "dans public/index.php !";
-
-
-
-
-
-
-/* TWIG */
-/* Variables */
-
-$test1 = "TEST 1 OK";
-$test2 = "TEST 2 OK";
-
-/* Conf */
+// Lien vers l'autoload
 require_once '../vendor/autoload.php';
 
+// Twig - emplacement du dossier des templates
 $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
+
+// Twig - chargement des variables d'envirronement
 $twig = new \Twig\Environment($loader, [
     'cache' => false,
 ]);
 
-
-/* Templates */
+// Twig - Emplacement du template à charger
 $template = $twig->load('index.html');
+
+// Twig - variables test
+$test1 = "TEST 1 OK";
+$test2 = "TEST 2 OK";
+
+// Twig - Rendu du template et des variables
 echo $template->render([
 	'test1' => $test1,
 	'test2' => $test2
 ]);
 
-/**
-	* place {{ test1 }} et {{ test2 }}
-	* dans ton template index.html
-	* pour vérifier que c'est ok
-	* Là tu as tout ce qu'il faut pour appeler tes templates
-	* et recupérer des variables php dans tes templates
-	*/
