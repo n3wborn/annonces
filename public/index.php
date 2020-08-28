0e8638f85b6014b41b9827cbdd8eebb7cf61d6ef
Stephane Pluchart <n3wborn@pm.me>
2020-08-28 10:34:25 +0200

    Add first routes

--- public/index.php
+++ public/index.php
@@ -2,25 +2,29 @@
 
 // Lien vers l'autoload
 require_once '../vendor/autoload.php';
+define("BASE_PATH", "");
+define("SERVER_URI", $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . ":" . "8080" . BASE_PATH) ;
+//define("SERVER_URI", $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . ":" . "$_SERVER['REMOTE_PORT']" . BASE_PATH) ;
+$router = new AltoRouter();
 
-// Twig - emplacement du dossier des templates
-$loader = new \Twig\Loader\FilesystemLoader('../application/templates');
+// Chargement de la page d'accueil
+$router->map( 'GET', '/', function() {
+	\App\Homepage::homepage();
+});
 
-// Twig - chargement des variables d'envirronement
-$twig = new \Twig\Environment($loader, [
-    'cache' => false,
-]);
+// Chargement de la page /user (exemple pour les prochaines pages)
+$router->map( 'GET', '/user', function( ) {
+	\App\Homepage::user();
+});
 
-// Twig - Emplacement du template à charger
-$template = $twig->load('index.html');
 
-// Twig - variables test
-$test1 = "TEST 1 OK";
-$test2 = "TEST 2 OK";
+// On verifie si ça match
+$match = $router->match();
 
-// Twig - Rendu du template et des variables
-echo $template->render([
-	'test1' => $test1,
-	'test2' => $test2
-]);
-
+// Action si match est true/false
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] );
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
