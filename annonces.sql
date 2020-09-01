c414669c594716fd19efc94beb3d978cc861db37
lzervini <lezervinia@gmail.com>
2020-09-01 14:11:34 +0200

    Update db

--- annonces.sql
+++ annonces.sql
@@ -1,28 +1,79 @@
-CREATE TABLE `annonces` (
-  `id` int(11) PRIMARY KEY,
-  `description` varchar(255),
-  `img_url` varchar(255),
-  `img_nom` varchar(255),
-  `est_validee` bool DEFAULT false,
-  `date_ecriture` date,
-  `date_validation` date,
-  `id_utilisateur` int(11),
-  `id_categorie` int(11)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 01 sep. 2020 à 12:09
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.4.0
 
-CREATE TABLE `categorie` (
-  `id` int(11) PRIMARY KEY,
-  `libelle` varchar(50)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `utilisateur` (
-  `id` int(11) PRIMARY KEY,
-  `courriel` varchar(255),
-  `nom` varchar(255),
-  `prenom` varchar(255),
-  `telephone` varchar(20)
-);
 
-ALTER TABLE `annonces` ADD FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-ALTER TABLE `annonces` ADD FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id`);
+--
+-- Base de données :  `annonces`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annonces`
+--
+
+DROP TABLE IF EXISTS `annonces`;
+CREATE TABLE IF NOT EXISTS `annonces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `img_url` varchar(255) DEFAULT NULL,
+  `img_nom` varchar(255) DEFAULT NULL,
+  `est_validee` tinyint(1) DEFAULT 0,
+  `date_ecriture` date DEFAULT NULL,
+  `date_validation` date DEFAULT NULL,
+  `id_utilisateur` int(11) DEFAULT NULL,
+  `id_categorie` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_utilisateur` (`id_utilisateur`),
+  KEY `id_categorie` (`id_categorie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courriel` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
