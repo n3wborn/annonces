af2926cd32e5afc17ac6837aaed482337be9f582
lzervini <lezervinia@gmail.com>
2020-09-04 09:36:52 +0200

    Add require to reCapatcha

--- public/js/recaptcha.js
+++ public/js/recaptcha.js
@@ -1,3 +1,11 @@
   var onloadCallback = function() {
     alert("grecaptcha is ready!");
   };
+
+  window.onload = function() {
+    var $recaptcha = document.querySelector('#g-recaptcha');
+
+    if($recaptcha) {
+        $recaptcha.setAttribute("required", "required");
+    }
+};
