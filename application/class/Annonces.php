16d3349d0ef0cb63b137221544217c57a3ba1dac
Stephane Pluchart <n3wborn@pm.me>
2020-09-07 13:46:12 +0200

    Add IsUser and InsertUser methods
    Add IsCategorie and InsertCategorie methods
    
    Add IsUser and InsertUser methods
    
    Add IsCategorie and InsertCategorie methods

--- application/class/Annonces.php
+++ application/class/Annonces.php
@@ -68,6 +68,266 @@ class Annonces extends Database
 
 
   /**
+   * @method InsertAnnonce()
+   * @return int|false
+   *
+   * InsertAnnonce insert une nouvelle annonce dans la table annonce.
+   * Elle retourne $id_annonce si true, false sinon
+   *
+   */
+
+  public function InsertAnnonce()
+  {
+    $dbh = $this->getPdo();
+
+  }
+
+
+  /**
+   * @method InsertUser()
+   * @param string $courriel
+   *
+   * InsertUser() créé un nouvel utilisateur
+   * renvoie son id si true, 0 sinon
+   */
+  public function InsertUser(string $courriel, string $nom, string $prenom, string $telephone) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    $sql = "INSERT INTO utilisateur(courriel, nom, prenom, telephone) VALUES (:courriel, :nom, :prenom, :telephone)";
+
+    // prepare & bind parameters
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':courriel', $courriel, PDO::PARAM_STR);
+    $sth->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $sth->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+    $sth->bindParam(':telephone', $telephone, PDO::PARAM_STR);
+
+    // exec & return 1 ou 0
+    if ($sth->execute()) {
+      return $dbh->lastInsertId();
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method IsUSer()
+   * @param string $courriel
+   *
+   * IsUser verifie si un utilisateur existe en prenant son courriel
+   * en parametre et retourne l'id utilisateur si true, 0 sinon
+   */
+  public function IsUser(string $courriel) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    $sql = "SELECT utilisateur.id FROM utilisateur WHERE utilisateur.courriel = :courriel";
+
+    // prepare & bind parameters
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':courriel', $courriel, PDO::PARAM_STR);
+    $sth->execute();
+
+    if ($resultat = $sth->fetch()) {
+      return(intval($resultat['id']));
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method IsCategorie()
+   * @param string $categorie
+   * @return int
+   *
+   * IsCategorie verifie si $categorie existe dans categorie.libelle
+   * Retourne l'id de la categorie correspondante si oui, 0 sinon.
+   *
+   */
+  public function IsCategorie(string $categorie) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    // evite les doublons dus aux majuscules
+    $categorie = strtolower($categorie);
+
+    // prepare & bind parameters
+    $sql = "SELECT categorie.id FROM categorie WHERE categorie.libelle = :categorie";
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':categorie', $categorie, PDO::PARAM_STR);
+    $sth->execute();
+
+    if ($resultat = $sth->fetch()) {
+      return(intval($resultat['id']));
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method InsertCategorie()
+   * @param string $categorie
+   * @return int
+   *
+   * InsertCategorie() insert $categorie dans categorie.libelle
+   * retourne l'id de la categorie, 0 sinon
+   */
+  public function InsertCategorie(string $categorie) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    // evite les doublons dus aux majuscules
+    $categorie = strtolower($categorie);
+
+    // prepare & bind parameters
+    $sql = "INSERT INTO categorie(libelle) VALUES (:categorie)";
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':categorie', $categorie, PDO::PARAM_STR);
+    $sth->execute();
+
+    if ($resultat = $sth->fetch()) {
+      return(intval($resultat['id']));
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method InsertAnnonce()
+   * @return int|false
+   *
+   * InsertAnnonce insert une nouvelle annonce dans la table annonce.
+   * Elle retourne $id_annonce si true, false sinon
+   *
+   */
+
+  public function InsertAnnonce()
+  {
+    $dbh = $this->getPdo();
+
+  }
+
+
+  /**
+   * @method InsertUser()
+   * @param string $courriel
+   *
+   * InsertUser() créé un nouvel utilisateur
+   * renvoie son id si true, 0 sinon
+   */
+  public function InsertUser(string $courriel, string $nom, string $prenom, string $telephone) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    $sql = "INSERT INTO utilisateur(courriel, nom, prenom, telephone) VALUES (:courriel, :nom, :prenom, :telephone)";
+
+    // prepare & bind parameters
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':courriel', $courriel, PDO::PARAM_STR);
+    $sth->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $sth->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+    $sth->bindParam(':telephone', $telephone, PDO::PARAM_STR);
+
+    // exec & return 1 ou 0
+    if ($sth->execute()) {
+      return $dbh->lastInsertId();
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method IsUSer()
+   * @param string $courriel
+   *
+   * IsUser verifie si un utilisateur existe en prenant son courriel
+   * en parametre et retourne l'id utilisateur si true, 0 sinon
+   */
+  public function IsUser(string $courriel) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    $sql = "SELECT utilisateur.id FROM utilisateur WHERE utilisateur.courriel = :courriel";
+
+    // prepare & bind parameters
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':courriel', $courriel, PDO::PARAM_STR);
+    $sth->execute();
+
+    if ($resultat = $sth->fetch()) {
+      return(intval($resultat['id']));
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method IsCategorie()
+   * @param string $categorie
+   * @return int
+   *
+   * IsCategorie verifie si $categorie existe dans categorie.libelle
+   * Retourne l'id de la categorie correspondante si oui, 0 sinon.
+   *
+   */
+  public function IsCategorie(string $categorie) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    // evite les doublons dus aux majuscules
+    $categorie = strtolower($categorie);
+
+    // prepare & bind parameters
+    $sql = "SELECT categorie.id FROM categorie WHERE categorie.libelle = :categorie";
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':categorie', $categorie, PDO::PARAM_STR);
+    $sth->execute();
+
+    if ($resultat = $sth->fetch()) {
+      return(intval($resultat['id']));
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
+   * @method InsertCategorie()
+   * @param string $categorie
+   * @return int
+   *
+   * InsertCategorie() insert $categorie dans categorie.libelle
+   * retourne l'id de la categorie, 0 sinon
+   */
+  public function InsertCategorie(string $categorie) : int
+  {
+    //connection à la bdd
+    $dbh = $this->getPdo();
+    // evite les doublons dus aux majuscules
+    $categorie = strtolower($categorie);
+
+    // prepare & bind parameters
+    $sql = "INSERT INTO categorie(libelle) VALUES (:categorie)";
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':categorie', $categorie, PDO::PARAM_STR);
+    $sth->execute();
+
+    if ($resultat = $sth->fetch()) {
+      return(intval($resultat['id']));
+    } else {
+      return 0;
+    }
+  }
+
+
+  /**
    * @method myParentSays() retourne l'objet PDO (Database private $pdo)
    * @return PDO Object
    */
