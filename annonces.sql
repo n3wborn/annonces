0028a5f477e74ddabe74fc98fabbfb7c6442847b
Stephane Pluchart <n3wborn@pm.me>
2020-09-07 10:29:52 +0200

    Update - fill database

--- annonces.sql
+++ annonces.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 01 sep. 2020 à 12:09
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.4.0
+-- Host: mariadb_1
+-- Generation Time: Sep 07, 2020 at 08:28 AM
+-- Server version: 10.4.13-MariaDB-1:10.4.13+maria~focal
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,18 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `annonces`
+-- Database: `annonces`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `annonces`
+-- Table structure for table `annonces`
 --
 
-DROP TABLE IF EXISTS `annonces`;
-CREATE TABLE IF NOT EXISTS `annonces` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `annonces` (
+  `id` int(11) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `img_url` varchar(255) DEFAULT NULL,
   `img_nom` varchar(255) DEFAULT NULL,
@@ -38,40 +36,123 @@ CREATE TABLE IF NOT EXISTS `annonces` (
   `date_ecriture` date DEFAULT NULL,
   `date_validation` date DEFAULT NULL,
   `id_utilisateur` int(11) DEFAULT NULL,
-  `id_categorie` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_utilisateur` (`id_utilisateur`),
-  KEY `id_categorie` (`id_categorie`)
+  `id_categorie` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `annonces`
+--
+
+INSERT INTO `annonces` (`id`, `description`, `img_url`, `img_nom`, `est_validee`, `date_ecriture`, `date_validation`, `id_utilisateur`, `id_categorie`) VALUES
+(1, 'description de l\'annonce', '/media/image.png', 'image.png', 0, '2020-09-02', NULL, 1, 2),
+(2, 'Four micro-onde', '/media/four.jpg', 'four.jpg', 0, '2020-09-02', NULL, 2, 6),
+(4, 'tes test etst', '/media/image.jpg', 'image.jpg', 1, '2020-09-04', '2020-09-05', 3, 4),
+(5, 'description de l annonce', '/medias/image.jpg', 'image.jpg', 0, '2020-09-07', NULL, 4, 7);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categorie`
+-- Table structure for table `categorie`
 --
 
-DROP TABLE IF EXISTS `categorie`;
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `libelle` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `libelle`) VALUES
+(1, 'Affaires professionnelles'),
+(2, 'Animaux'),
+(3, 'Auto-Moto'),
+(4, 'Emploi'),
+(5, 'Immobilier'),
+(6, 'Services'),
+(7, 'Vacances');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
   `courriel` varchar(255) DEFAULT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `prenom` varchar(255) DEFAULT NULL,
-  `telephone` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `telephone` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `courriel`, `nom`, `prenom`, `telephone`) VALUES
+(1, 'JohnDoe@domain.com', 'Doe', 'John', '06 06 06 06 06'),
+(2, 'test@domain.org', 'bidule', 'machine', '06 06 06 07 07'),
+(3, 'courriel@gmail.com', 'alachaussurenoir', 'Legrandblond', '07 07 07 07 07'),
+(4, 'mail@maildomain.com', 'noname', 'nonick', '03 44 55 99 77');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`),
+  ADD KEY `id_categorie` (`id_categorie`);
+
+--
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `annonces`
+--
+ALTER TABLE `annonces`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD CONSTRAINT `annonces_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `annonces_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
