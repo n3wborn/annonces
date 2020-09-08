8e648f059ef4b137b6fe60647e8da9f16e4e8b0e
lzervini <lezervinia@gmail.com>
2020-09-08 15:51:00 +0200

    Update DB

--- annonces.sql
+++ annonces.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: mariadb_1
--- Generation Time: Sep 07, 2020 at 08:28 AM
--- Server version: 10.4.13-MariaDB-1:10.4.13+maria~focal
--- PHP Version: 7.4.6
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 08 sep. 2020 à 13:49
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.4.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,17 +19,19 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `annonces`
+-- Base de données :  `annonces`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `annonces`
+-- Structure de la table `annonces`
 --
 
-CREATE TABLE `annonces` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `annonces`;
+CREATE TABLE IF NOT EXISTS `annonces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prix` int(11) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `img_url` varchar(255) DEFAULT NULL,
   `img_nom` varchar(255) DEFAULT NULL,
@@ -36,32 +39,41 @@ CREATE TABLE `annonces` (
   `date_ecriture` date DEFAULT NULL,
   `date_validation` date DEFAULT NULL,
   `id_utilisateur` int(11) DEFAULT NULL,
-  `id_categorie` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_categorie` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_utilisateur` (`id_utilisateur`),
+  KEY `id_categorie` (`id_categorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `annonces`
+-- Déchargement des données de la table `annonces`
 --
 
-INSERT INTO `annonces` (`id`, `description`, `img_url`, `img_nom`, `est_validee`, `date_ecriture`, `date_validation`, `id_utilisateur`, `id_categorie`) VALUES
-(1, 'description de l\'annonce', '/media/image.png', 'image.png', 0, '2020-09-02', NULL, 1, 2),
-(2, 'Four micro-onde', '/media/four.jpg', 'four.jpg', 0, '2020-09-02', NULL, 2, 6),
-(4, 'tes test etst', '/media/image.jpg', 'image.jpg', 1, '2020-09-04', '2020-09-05', 3, 4),
-(5, 'description de l annonce', '/medias/image.jpg', 'image.jpg', 0, '2020-09-07', NULL, 4, 7);
+INSERT INTO `annonces` (`id`, `prix`, `description`, `img_url`, `img_nom`, `est_validee`, `date_ecriture`, `date_validation`, `id_utilisateur`, `id_categorie`) VALUES
+(1, 56, 'description de l\'annonce', '/media/image.png', 'image.png', 0, '2020-09-02', NULL, 1, 2),
+(2, 34, 'Four micro-onde', '/media/four.jpg', 'four.jpg', 0, '2020-09-02', NULL, 2, 6),
+(4, 23, 'tes test etst', '/media/image.jpg', 'image.jpg', 1, '2020-09-04', '2020-09-05', 3, 4),
+(5, 12, 'description de l annonce', '/medias/image.jpg', 'image.jpg', 0, '2020-09-07', NULL, 4, 7),
+(6, 65, 'description de l\'annonce', '/media/image.png', 'image.png', 0, '2020-09-02', NULL, 1, 2),
+(7, 47, 'Four micro-onde', '/media/four.jpg', 'four.jpg', 0, '2020-09-02', NULL, 2, 6),
+(8, 57, 'tes test etst', '/media/image.jpg', 'image.jpg', 1, '2020-09-04', '2020-09-05', 3, 4),
+(9, 12, 'description de l annonce', '/medias/image.jpg', 'image.jpg', 0, '2020-09-07', NULL, 4, 7);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorie`
+-- Structure de la table `categorie`
 --
 
-CREATE TABLE `categorie` (
-  `id` int(11) NOT NULL,
-  `libelle` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `categorie`
+-- Déchargement des données de la table `categorie`
 --
 
 INSERT INTO `categorie` (`id`, `libelle`) VALUES
@@ -76,19 +88,21 @@ INSERT INTO `categorie` (`id`, `libelle`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateur`
+-- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `courriel` varchar(255) DEFAULT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `prenom` varchar(255) DEFAULT NULL,
-  `telephone` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `telephone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `utilisateur`
+-- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id`, `courriel`, `nom`, `prenom`, `telephone`) VALUES
@@ -98,57 +112,11 @@ INSERT INTO `utilisateur` (`id`, `courriel`, `nom`, `prenom`, `telephone`) VALUE
 (4, 'mail@maildomain.com', 'noname', 'nonick', '03 44 55 99 77');
 
 --
--- Indexes for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Indexes for table `annonces`
---
-ALTER TABLE `annonces`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`),
-  ADD KEY `id_categorie` (`id_categorie`);
-
---
--- Indexes for table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `annonces`
---
-ALTER TABLE `annonces`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `annonces`
+-- Contraintes pour la table `annonces`
 --
 ALTER TABLE `annonces`
   ADD CONSTRAINT `annonces_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
