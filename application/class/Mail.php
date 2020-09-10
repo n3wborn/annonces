577980143acce68c9bee89e9935d69322dc8260f
lzervini <lezervinia@gmail.com>
2020-09-10 18:30:48 +0200

    Change Username + PXD

--- application/class/Mail.php
+++ application/class/Mail.php
@@ -28,13 +28,13 @@ class Mail
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.mail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'dd3a76c21395d4';                     // SMTP username
-            $mail->Password   = '9a5f410c988e01';                               // SMTP password
+            $mail->Username   = 'lea.zervini@mail.com';                     // SMTP username
+            $mail->Password   = '**';                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         
             //Recipients
-            $mail->setFrom('dd3a76c21395d4', 'ELEPHADS');
+            $mail->setFrom('lea.zervini@mail.com', 'ELEPHADS');
             $mail->addAddress($mailto, $prenom . ' ' . $nom);    
             
             // Content
