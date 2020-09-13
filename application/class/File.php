06f3bebf91488fe77f5395366847dea4722d548f
n3wborn <n3wborn@protonmail.com>
2020-09-13 13:01:12 +0200

    Add Mime check to uploadFile method

--- application/class/File.php
+++ application/class/File.php
@@ -4,9 +4,10 @@ namespace App;
 
 use \App\Annonces as Annonces;
 use \App\Database as Database;
+use \App\Crypt as Crypt;
 
 
-class File extends Database
+class File extends Annonces
 {
 
   public  function __construct()
@@ -25,19 +26,27 @@ class File extends Database
   public static function checkMimeType(string $file)
   {
     // type authorisés
+    // $mime_types = array(
+    //   'image/png' => "png",
+    //   'image/jpeg'=> "jpeg",
+    //   'image/jpg'=> "jpg",
+    //   'image/gif' => "gif",
+    //   'image/bmp' => "bmp"
+    // );
+
     $mime_types = array(
-      'image/png' => "png",
-      'image/jpeg'=> "jpeg",
-      'image/jpg'=> "jpg",
-      'image/gif' => "gif",
-      'image/bmp' => "bmp"
+      'image/png',
+      'image/jpeg',
+      'image/jpg',
+      'image/gif',
+      'image/bmp'
     );
 
     // on regarde le type mime du fichier
     $image_mime = image_type_to_mime_type(exif_imagetype($file));
 
-    // on verifie que le type Mime fase pâsrtie de ceux autorisés
-    if (!array_key_exists($image_mime, $mime_types)) {
+    // on verifie que le type Mime fasse pas partie de ceux autorisés
+    if (!in_array($image_mime, $mime_types)) {
       return false;
     } else {
       return true;
@@ -50,26 +59,29 @@ class File extends Database
    * @method uploadFile
    * @return str $nom
    *
-   * uploadFile() prend va recupérer le fichier du formulaire d'ajout
-   * l'uploader et retourner le nom du fichier
+   * uploadFile() va recupérer le fichier du formulaire d'ajout, vérifier si le
+   * type Mime est autorisé et retourner le nom du fichier, sinon on utilise
+   * l'image par defaut
    */
   public static function uploadFile()
   {
     if(isset($_FILES) && ($_FILES['img_nom']['size'] < 300000 && $_FILES['img_nom']['error'] === 0)) {
 
-      // si upload ok
+      // recupere le nom du fichier temporaire
       $tmp = $_FILES['img_nom']['tmp_name'];
-      $nom = basename($_FILES['img_nom']['name']);
 
-      // place le fichier dans le repertoire assets et renvoie le nom du fichier
-      if (move_uploaded_file($tmp, "assets/$nom")) {
-        return "$nom";
-      } else {
-        echo "Erreur dans l'envoi du fichier";
-        die();
+      // verifie si le type mime fait partie de ceux autorisés
+      if (File::checkMimeType($tmp)) {
+
+        $nom = basename($_FILES['img_nom']['name']);
+        $uuid = Crypt::getRandStr(5);
+        $nom = $uuid . $nom;
+
+        // si oui, deplace le fichier temporaire en lui donnant le nouveau nom
+        if (move_uploaded_file($tmp, "assets/$nom")) {
+          return $nom;
+        }
       }
     }
   }
-
-
 }
