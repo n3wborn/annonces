a83e924f0d8155bbe7d64cf68e05c780809c7d4e
Stephane Pluchart <n3wborn@pm.me>
2020-09-14 15:53:46 +0200

    Update explodeUrl() to handle REQUEST_URI

--- application/class/Crypt.php
+++ application/class/Crypt.php
@@ -124,12 +124,12 @@ class Crypt
   public static function explodeUrl($url)
   {
     $parts = explode('/', $url);
-
+    var_dump($parts);
     $infos = [
-    'action' => $parts[2],
-    'ciphered_uuid' => $parts[3],
-    'ciphered_mail' => $parts[4],
-    'hash' => $parts[5]
+    'action' => $parts[1],
+    'ciphered_uuid' => $parts[2],
+    'ciphered_mail' => $parts[3],
+    'hash' => $parts[4]
     ];
 
     return $infos;
