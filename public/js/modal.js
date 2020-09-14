fb32d7b8d56ed964a103ad1408bd6a59309fb86d
lzervini <lezervinia@gmail.com>
2020-09-14 10:06:14 +0200

    Add modal with redirection

--- public/js/modal.js
+++ public/js/modal.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
 		});
 	}); 
 });
+
+// DELETE BUTTON
+
+const deleteLinks = document.getElementsByClassName('btn_delete');
+
+for (deleteLink of deleteLinks){
+    //Affecte l'évenement click 
+    //Sur clic executera une fonction sans nom dite anonyme
+    deleteLink.addEventListener('click', function(e){
+        e.preventDefault();
+    
+    //Selectionne l'élement ayant l'id modal
+    const modal = document.getElementById('modal');
+	modal.classList.remove('hidden');
+	
+	window.setTimeout("location=('/');",2500);
+});
+}
